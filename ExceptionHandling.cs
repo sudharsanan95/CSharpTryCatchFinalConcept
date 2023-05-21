cc53e96393c32a6180b89e5060aef3780b2170be
Sudharsanan <sudharsananramesh0312@gmail.com>
2023-05-21 13:54:50 +0530

    Add project files.

---
+++ ExceptionHandling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinalConcept
+{
+    public class ExceptionHandling
+    {
+        int id;
+        string itemName;
+        int tax;
+
+        public ExceptionHandling()
+        {
+            Console.WriteLine("server is connected to database");
+        }
+        public void DisplayMenuDetails(int id, string itemName, int tax)
+        {
+            try
+            {
+                this.id = id;
+                this.itemName = itemName;
+                this.tax = tax;
+
+                Console.WriteLine(id);
+                Console.WriteLine(itemName);
+                Console.WriteLine(180 / tax);
+            }
+            catch
+            {
+                Console.WriteLine("Hi, i am getting exception");
+            }
+            finally
+            {
+                Console.WriteLine("Enjoy your Lunch");
+                Console.WriteLine("server is disconnected from database");
+            }
+           
+        }
+    }
+        }
+    
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinalConcept
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            ExceptionHandling ex = new ExceptionHandling();
+            ex.DisplayMenuDetails(005, "Briyani", 0);
+
+        }
+    }
+}
